82e0c5b60c06e259e6ac83d360bb091b44984582
jhoffmann <jens.hoffmann@lidl.com>
2016-06-20 14:54:33 +0200

    * added some code.

--- src/main/java/schultz/dustin/io/JustGifItApplication.java
+++ src/main/java/schultz/dustin/io/JustGifItApplication.java
@@ -3,7 +3,9 @@ package schultz.dustin.io;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -27,6 +29,13 @@ public class JustGifItApplication {
         if (!gifFolder.exists()) {
             gifFolder.mkdir();
         }
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterHiddenHttpMethodFilter(HiddenHttpMethodFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
     }
 
     @Bean
